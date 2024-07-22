7c8aa24dbeb804fea6057cedd34d0169a56d34bf
zihan_cityu <32484940+wzh4464@users.noreply.github.com>
2024-07-22 20:46:54 +0800

    build

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@
  * Created Date: Thursday, July 18th 2024
  * Author: Zihan
  * -----
- * Last Modified: Thursday, 18th July 2024 4:47:50 pm
+ * Last Modified: Monday, 22nd July 2024 8:43:07 pm
  * Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
  * -----
  * HISTORY:
@@ -15,13 +15,14 @@ use std::env;
 use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
+extern crate pkg_config;
 
 fn main() {
     // 输出所有环境变量到 stderr
     for (key, value) in env::vars() {
         eprintln!("{}: {}", key, value);
     }
-    
+
     // 确认 make 工具在系统路径中
     let make = env::var("MAKE").unwrap_or_else(|_| "make".to_string());
 
@@ -36,20 +37,46 @@ fn main() {
         panic!("Failed to build ELSDc_c library");
     }
 
-    // 移动 libelsdc.so 到当前文件夹
+    // 移动 libelsdc.so 或 libelsdc.dylib 到当前文件夹
     let current_dir = env::current_dir().expect("Failed to get current directory");
-    let src = current_dir.join("ELSDc_c").join("libelsdc.so");
-    let dest = current_dir.join("libelsdc.so");
-    fs::rename(&src, &dest).expect("Failed to move libelsdc.so to current directory");
+    let src = if cfg!(target_os = "macos") {
+        current_dir.join("ELSDc_c").join("libelsdc.dylib")
+    } else {
+        current_dir.join("ELSDc_c").join("libelsdc.so")
+    };
+    let dest = if cfg!(target_os = "macos") {
+        current_dir.join("libelsdc.dylib")
+    } else {
+        current_dir.join("libelsdc.so")
+    };
+    fs::rename(&src, &dest).expect("Failed to move libelsdc library to current directory");
 
     // 告诉 cargo 链接编译后的共享库
     println!("cargo:rustc-link-search=native={}", current_dir.display());
     println!("cargo:rustc-link-lib=dylib=elsdc");
 
+    // 确认 OpenCV 库
+    let opencv = if cfg!(target_os = "macos") {
+        pkg_config::Config::new().probe("opencv4").unwrap()
+    } else {
+        pkg_config::Config::new().probe("opencv4").unwrap_or_else(|_| pkg_config::Config::new().probe("opencv").unwrap())
+    };
+
+    for path in opencv.link_paths {
+        println!("cargo:rustc-link-search=native={}", path.display());
+    }
+    for lib in opencv.libs {
+        println!("cargo:rustc-link-lib={}", lib);
+    }
+    for framework in opencv.frameworks {
+        println!("cargo:rustc-link-lib=framework={}", framework);
+    }
+
     // 生成绑定代码
     let bindings = bindgen::Builder::default()
         .header("ELSDc_c/src/elsdc.h")
         .clang_arg("-IELSDc_c/src")
+        .clang_args(opencv.include_paths.iter().map(|path| format!("-I{}", path.display())))
         .generate()
         .expect("Failed to generate bindings");
 
